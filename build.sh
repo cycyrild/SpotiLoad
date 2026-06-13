d74dcbaf690503651e003012cfa688951dadc6e4
Cyril DRIMARACCI <cyrildrimaracci@live.fr>
2026-06-14 00:39:36 +0200

    refactor: introduce nt-crtless-runtime and update build configurations

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
 
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release
-cmake --build build --clean-first
+cmake --preset default
+cmake --build --preset default --clean-first -v
